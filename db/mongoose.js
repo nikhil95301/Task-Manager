e2c3bbd6f481aa0fc7f12bba603cf02f504c592d
nikhil95301 <nikhil9530175113@gmail.com>
2020-08-02 14:53:42 +0530

    Hello

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 //console.log(process.env.mongo)
-mongoose.connect(process.env.mongo,   {
+mongoose.connect(process.env.m,   {
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true
