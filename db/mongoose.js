7edde95f0aa13ec83a2bbcd24dd174bc2f33d0e1
nikhil95301 <nikhil9530175113@gmail.com>
2020-08-02 14:56:42 +0530

    hello

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 //console.log(process.env.mongo)
-mongoose.connect(process.env.m,   {
+mongoose.connect("mongodb+srv://nikhilakg:Nikhil1810013@cluster0.dnspg.mongodb.net/<dbname>?retryWrites=true&w=majority",   {
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true
